f71ace43d81790f0b44373c34a75817fceec75c7
APS69 <APS69@APS.local>
2025-05-30 17:02:05 -0300

    Adição do ServiceWorker

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { VueTheMask } from 'vue-the-mask'
+import './registerServiceWorker'
 
 const app = createApp(App)
 app.use(router)
